66601da150ae0ac14eecd731a9a11f6fd281c74d
Meherab Hasan Fahim <fahim2305341670@diu.edu.bd>
2025-09-30 22:22:33 +0600

    Update wholesaleBuyerRoutes.js

--- routes/wholesaleBuyerRoutes.js
+++ routes/wholesaleBuyerRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { protect } = require('../middleware/authMiddleware');
 
 // Import all the functions from the controller
 const {
